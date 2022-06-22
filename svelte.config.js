90643e62c7143742a6b0805b02993f3b9ed2b08d
Chris Cardoza <chris.siya511@gmail.com>
2022-06-22 01:57:46 -0400

    tailwind config files and small test

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import mdsvexConfig from './mdsvex.config';
+import mdsvexConfig from './mdsvex.config.js';
 import md from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [md.mdsvex(mdsvexConfig)],
+	preprocess: [preprocess({ postcss: true }), md.mdsvex(mdsvexConfig)],
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	kit: {
 		adapter: adapter()
